800282ac54a18886e65dbfe9612ba5d890a7e1e3
Jaime <you@example.com>
2021-12-09 20:19:29 -0500

    First version

---
+++ src/main/java/pe/edu/tecsup/tienda/SpringTiendaWeb2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTiendaWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTiendaWeb2Application.class, args);
+	}
+
+}
