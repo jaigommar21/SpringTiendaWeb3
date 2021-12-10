fe6c43cd34d972bed2ebc8d2b796bb0abc5ad6d1
Jaime <you@example.com>
2021-12-09 20:59:19 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/repositories/CategoriaRepository.java
+++ src/main/java/pe/edu/tecsup/tienda/repositories/CategoriaRepository.java
@@ -3,12 +3,13 @@ package pe.edu.tecsup.tienda.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import pe.edu.tecsup.tienda.entities.Categoria;
 
 public interface CategoriaRepository 
-		extends CrudRepository<Categoria, Long> {
+		extends JpaRepository<Categoria, Long> {
 
 	// CRUD : Create, Read , Update and Delete 
 	
--- src/main/java/pe/edu/tecsup/tienda/repositories/ProductoRepository.java
+++ src/main/java/pe/edu/tecsup/tienda/repositories/ProductoRepository.java
@@ -2,12 +2,13 @@ package pe.edu.tecsup.tienda.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import pe.edu.tecsup.tienda.entities.Producto;
 
 public interface ProductoRepository 
-		extends CrudRepository<Producto, Long> {
+		extends JpaRepository<Producto, Long> {
 
 	// CRUD : Create, Read , Update and Delete 
 	
