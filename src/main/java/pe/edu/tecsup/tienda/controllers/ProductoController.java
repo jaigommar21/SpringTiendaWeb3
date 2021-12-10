d8927ea59210869da9a855b01d30321214bb5113
Jaime <you@example.com>
2021-12-09 21:53:01 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
@@ -1,5 +1,6 @@
 package pe.edu.tecsup.tienda.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,8 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pe.edu.tecsup.tienda.entities.Categoria;
 import pe.edu.tecsup.tienda.entities.Producto;
@@ -46,6 +53,7 @@ public class ProductoController {
 
 	@GetMapping("/create")
 	public String create(Model model) throws Exception {
+		
 		logger.info("call create()");
 		
 		List<Categoria> categorias = categoriaService.findAll();
@@ -56,5 +64,25 @@ public class ProductoController {
 		
 		return "productos/create";
 	}
+	
+	@PostMapping("/store")
+	public String store(@ModelAttribute("producto") Producto producto, Errors errors, 
+			@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttrs) throws Exception{
+
+		logger.info("call store(producto: " + producto + ")");
+		
+		//producto.setCreado(new Date());
+		
+		producto.setEstado(1);  // Activo 
+		
+		productoService.save(producto); // Save
+		
+		redirectAttrs.addFlashAttribute("message", "Registro guardado correctamente");
+		
+		return "redirect:/productos/";
+	}
+	
+
 
 }
