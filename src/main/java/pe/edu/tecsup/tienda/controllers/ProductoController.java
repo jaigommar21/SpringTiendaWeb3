eee26dcdb18ffdd05219ca92b3b2fdcc5dc54bab
Jaime <you@example.com>
2021-12-14 19:10:48 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
@@ -1,6 +1,5 @@
 package pe.edu.tecsup.tienda.controllers;
 
-import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -83,6 +83,17 @@ public class ProductoController {
 		return "redirect:/productos/";
 	}
 	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Long id, RedirectAttributes redirectAttrs) throws Exception {
+		
+		logger.info("Delete(id: " + id + ")");
+		
+		productoService.deleteById(id);
+		
+		redirectAttrs.addFlashAttribute("message", "Registro eliminado correctamente");
+		
+		return "redirect:/productos/";
+	}
 
 
 }
