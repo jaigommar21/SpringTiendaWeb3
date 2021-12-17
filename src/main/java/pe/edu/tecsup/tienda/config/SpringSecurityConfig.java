33aa836e195077b3ce2e5b500ebc7b817381a5d0
Jaime <you@example.com>
2021-12-16 20:14:04 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -51,6 +52,26 @@ public class SpringSecurityConfig
 	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService);
 	}
+
+	
+	 @Override
+	    protected void configure(final HttpSecurity http) throws Exception {
+	        http
+	        // Change login
+	        	.formLogin()
+	        	.loginPage("/login")
+	        	.loginProcessingUrl("/authenticate")
+	        	.defaultSuccessUrl("/")
+	        	.failureUrl("/login?error")
+	        	.usernameParameter("username").passwordParameter("password")
+	    	.and()
+	    	// Change logout
+	        	.logout()
+	        	.logoutUrl("/logout")
+	        	.logoutSuccessUrl("/login")
+	        .and()
+		.csrf().disable();
+	    }
 
 	
 }
